1c9c2ad5e1ea52cfbdc0c710eaa56bb5e92b30d2
bycarlosgamez <hardpx@gmail.com>
2025-11-14 12:42:19 -0600

    setup and serch component

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,35 @@
+import { useState, type ChangeEvent, type FormEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { VscSearch } from 'react-icons/vsc';
+
+function SearchInput() {
+  const [term, setTerm] = useState('');
+  const navigate = useNavigate();
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTerm(e.target.value);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    navigate(`/search?=${term}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className='relative'>
+        <div className='absolute inset-y-0 flex items-center pl-3'>
+          <VscSearch className='h-5 text-gray-500' />
+        </div>
+        <input
+          className='pl-10 py-2 w-full border-0 shadow-none'
+          value={term}
+          onChange={handleChange}
+          placeholder='Search'
+        />
+      </div>
+    </form>
+  );
+}
+
+export default SearchInput;
